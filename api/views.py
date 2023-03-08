cb759ece1cf43614c0a9e001a92cbd2c7150b158
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-07 19:33:53 -0500

    added views for models

--- api/views.py
+++ api/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-def main(request):
-    return HttpResponse("hello world")
+from rest_framework import generics
+from .serializers import GenreSerializer, BandSerializer, VideoSerializer
+from .models import Genre, Band, Video
+
+
+class GenreView(generics.CreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class BandView(generics.CreateAPIView):
+    queryset = Band.objects.all()
+    serializer_class = BandSerializer
+
+class VideoView(generics.CreateAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
--- metal_discovery/urls.py
+++ metal_discovery/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),  # change made here
+    path('api/', include('api.urls')),  # change made here
 ]
