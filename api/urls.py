0818ec7246c7adcb4259665948f55b6d3976b648
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-07 19:31:16 -0500

    created endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
-
 from django.urls import path
-from .views import main
+from .views import GenreView, BandView, VideoView
 
 urlpatterns = [
-    path('', main),
+    path('genres/', GenreView.as_view(), name='genre_list'),
+    path('bands/', BandView.as_view(), name='band_list'),
+    path('videos/', VideoView.as_view(), name='video_list'),
 ]
