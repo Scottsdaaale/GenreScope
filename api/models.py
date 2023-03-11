5d90c16db1cdb806c44064158f0cd941f93ec3a6
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-10 19:10:49 -0500

    testing metal_genres

--- api/models.py
+++ api/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+import metal_genres
+
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=100)
+    metal_genre = models.CharField(max_length=20, choices=metal_genres)
 
     def __str__(self):
         return self.name
--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('genres/', GenreView.as_view(), name='genre_list'),
     path('bands/', BandView.as_view(), name='band_list'),
     path('videos/', VideoView.as_view(), name='video_list'),
-    path('tracks/', TrackView.as_view(), name='track_detail'), 
+    path('tracks/', TrackView.as_view(), name='track_detail'),
 ]
