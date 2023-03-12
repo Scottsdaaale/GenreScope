aba29264b444c9b0b81f80b675c8e55c205fbbdb
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-12 14:08:49 -0400

    added search url and hello for testing

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import get_genres
+# from .views import get_genres
+from .views import hello, search_spotify
 
 urlpatterns = [
-    path('genres/', get_genres, name='genres'),
+    # path('genres/', get_genres, name='genres'),
+    path('hello/', hello, name='hello'),
+    path('search/', search_spotify, name='search'),
 ]
