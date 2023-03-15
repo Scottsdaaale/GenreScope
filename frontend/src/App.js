1d9fea9f93ee06df414d33393cea0080a339b7a2
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-14 20:06:32 -0400

    returned genre component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import Genres from "./Components/Genres";
 
 function App() {
 
-  const [data, setData] = useState("")
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/genres/")
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        // do something with the data here
-      })
-      .catch((error) => console.error(error));
-  }, []);
-  console.log(data)
-
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Genres/>
+    </div>
+  );
 }
 
 export default App;
-
 
 //https://www.youtube.com/watch?v=_iOGtzRXwhI
