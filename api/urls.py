88a4615418e8f7aba8c51a89a3d88b53a9a813b9
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-18 20:29:19 -0400

    created path for csrf token

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import search_spotify, genres
+from .views import search_spotify, genres, csrf , ping
 
 urlpatterns = [
     path('search/', search_spotify, name='search'),
-    path('genres/', genres, name='genres')
+    path('genres/', genres, name='genres'),
+    path('csrf/', csrf, name='csrf'),
+    path('ping/', ping, name='ping'),
 ]
