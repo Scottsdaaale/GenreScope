f3d3be6a837e2f9bcf99333ddcda8bd633d09d84
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-18 20:32:02 -0400

    fetch token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
 import Genres from "./Components/Genres";
 
+
+
 function App() {
 
+
+  fetch('/api/csrf/')
+  .then(response => response.json())
+  .then(data => {
+    const token = (data.csrfToken)
+    // add csrfToken to your POST request headers
+  })
+  .catch(error => console.error(error));
+  
   return (
     <div>
       <Genres/>
