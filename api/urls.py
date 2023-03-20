7375b8dc8fae4635f353864f69e4ccad90b1cda4
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-20 19:57:29 -0400

    comment out unused paths

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import search_spotify, genres, csrf , ping
+from .views import search_spotify, genres
+# csrf , 
+# ping, 
+# get_csrf_token
 
 urlpatterns = [
     path('search/', search_spotify, name='search'),
     path('genres/', genres, name='genres'),
-    path('csrf/', csrf, name='csrf'),
-    path('ping/', ping, name='ping'),
+    # path('csrf/', csrf, name='csrf'),
+    # path('ping/', ping, name='ping'),
+    # path('get_csrf_token/', get_csrf_token, name='get_csrf_token'),
 ]
