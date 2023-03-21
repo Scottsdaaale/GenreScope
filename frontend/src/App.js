eff8ab16199826be43ba7e8c345742b336f08da1
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-20 22:28:24 -0400

    added react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import Genres from "./Components/Genres";
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 
 
 function App() {
-
-  
   return (
-    <div>
-      <Genres/>
-    </div>
+   <div><Genres/></div>
   );
 }
 
