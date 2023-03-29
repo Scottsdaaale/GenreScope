86e79977af733e09698f7a1d4b2ff9560558d0d5
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-28 22:28:18 -0400

    render artists name, top track, and preview url

--- frontend/src/Components/Artists.js
+++ frontend/src/Components/Artists.js
@@ -1,39 +1,24 @@
-// function Artists(props) {
-//   const searchResult = props.location.state.searchResult;
-//   console.log(searchResult);
-//   if (!searchResult) {
-//     return <p>No search results to display.</p>;
-//   }
-//   return (
-//     <div>
-//       <h2>Artists</h2>
-//       {searchResult.length > 0 ? (
-//         searchResult.map((artist) => (
-//           <div key={artist.id}>
-//             <h3>{artist.name}</h3>
-//             <p>{artist.popularity}</p>
-//             <ul>
-//               {artist.top_tracks.map((track) => (
-//                 <li key={track.id}>{track.name}</li>
-//               ))}
-//             </ul>
-//           </div>
-//         ))
-//       ) : (
-//         <p>No search results to display.</p>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Artists;
 import React from "react";
+import { useLocation } from "react-router-dom";
 
-function Artists({ searchResult }) {
-  console.log(searchResult);
+function Artists() {
+  const location = useLocation()
+  const artist = location.state.data.artists[0]
+  console.log(artist)
+  const topTrackName = artist.top_tracks[0].name
+  const topTrack = artist.top_tracks[0].preview_url
+  console.log(location.state.data)
+
   return (
     <div>
+      <h1>{artist.name}</h1>
+      <h2>{topTrackName}</h2>
+      <audio controls>
+        <source src={topTrack} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
     </div>
+
   );
 }
 
