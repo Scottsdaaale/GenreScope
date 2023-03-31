e75efe0d3bb17ce8f1a0701bbb00c00b891f2439
Scottsdaaale <scottsdaaale@gmail.com>
2023-03-30 22:50:59 -0400

    added new urls

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import search_spotify, genres
+from .views import genres, search_artists, search_top_tracks
 # csrf , 
 # ping, 
 # get_csrf_token
 
 urlpatterns = [
-    path('search/', search_spotify, name='search'),
+    # path('search/', search_spotify, name='search'),
+    path('artists/', search_artists, name='artists'),
+    path('tracks/', search_top_tracks, name='tracks'),
     path('genres/', genres, name='genres'),
     # path('csrf/', csrf, name='csrf'),
     # path('ping/', ping, name='ping'),
