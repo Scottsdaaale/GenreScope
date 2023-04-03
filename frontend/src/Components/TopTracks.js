d5c8c9d2e79dafbdbd8b2d468b9a2d01ed77ea85
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-03 17:53:32 -0400

    added top tracks page

--- frontend/src/Components/TopTracks.js
+++ frontend/src/Components/TopTracks.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 function TopTracks() {
+  const location = useLocation();
+  const tracks = location.state.data.tracks;
+  console.log(tracks);
+
   return (
-    <div>TopTracks</div>
-  )
+    <div>
+      {tracks.map((track) => (
+        <ul key={track.id}>
+          <li>
+            {track.name}
+            {track.preview_url ? (
+              <audio controls>
+                <source src={track.preview_url} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+            ) : (
+              <p>No preview available.</p>
+            )}
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
 }
 
-export default TopTracks
+export default TopTracks;
