6bcd839afa6b762b461745d3abad717d8ec1e0cb
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-03 17:38:33 -0400

    improved search results

--- api/views.py
+++ api/views.py
@@ -3,12 +3,14 @@ import requests
 from django.http import JsonResponse, HttpResponse
 from .metal_genres import metal_genres
 from .oauth import access_token
+from .filter_genres import filtered_genres
 
 
 def search_artists(request):
     if request.method == "POST":
         data = json.loads(request.body)
         genre = data.get("genre")
+        unwanted_genres = filtered_genres['genres'] # list of unwanted genres
         query_params = {
             "q": genre,
             "type": "artist",
@@ -25,14 +27,23 @@ def search_artists(request):
             artists = results["artists"]["items"]
             sorted_artists = sorted(artists, key=lambda x: x["popularity"], reverse=True)
             response_data = {"total": results["artists"]["total"], "artists": []}
+            num_removed = 0  # keep track of how many artists are removed
             for artist in sorted_artists:
+                if any(genre in artist["genres"] for genre in unwanted_genres):
+                    num_removed += 1
+                    continue  # skip artist if they have any unwanted genre
                 artist_data = {
                     "name": artist["name"],
                     "id": artist["id"],
                     "image_url": artist["images"][0]["url"] if artist["images"] else None,
                     "popularity": artist["popularity"],
+                    "genres": artist["genres"]
                 }
                 response_data["artists"].append(artist_data)
+                if len(response_data["artists"]) == 50:
+                    break  # stop adding artists if we already have 50
+            response_data["total"] = min(len(response_data["artists"]), 50)  # adjust the total to account for removed artists
+            query_params["limit"] = len(response_data["artists"]) + num_removed  # adjust the limit to account for removed artists
             return JsonResponse(response_data)
         else:
             print(f"Request failed with status code {response.status_code}")
@@ -81,3 +92,4 @@ def genres(request):
 
 # def get_csrf_token(request):
 #     return JsonResponse({'csrfToken': request.COOKIES['csrftoken']})
+
