4c0534abf940405eaec353bb86085c2d64817324
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-05 17:53:10 -0400

    added results page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Genres from "./Components/Genres"
 import Artists from "./Components/Artists"
 import TopTracks from "./Components/TopTracks"
+import Results from "./Components/Results"
 import { Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Genres />} />
+      <Route path='/results' element={<Results />} />
       <Route path='/genres/:genre' element={<Artists />} />
-      <Route path='/:artist' element={<TopTracks />} />
+      <Route path='/artists/:artist' element={<TopTracks />} />
     </Routes>
   );
 }
