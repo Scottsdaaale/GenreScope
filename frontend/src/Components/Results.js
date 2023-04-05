668157084ccbaeef928a9647e17178abcccaa289
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-05 17:53:49 -0400

    created results page

---
+++ frontend/src/Components/Results.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Genres from "./Components/Genres"
+// import Artists from "./Components/Artists"
+// import TopTracks from "./Components/TopTracks"
+
+function Results() {
+  return (
+    <div>
+        {/* <Artists/> */}
+        {/* <Genres/> */}
+    </div>
+  )
+}
+
+export default Results
