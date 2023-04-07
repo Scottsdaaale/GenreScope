8857ad403ae50ee057b63a04dd807c350d0b2e6b
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-06 21:10:17 -0400

    pass data

--- frontend/src/Components/Results.js
+++ frontend/src/Components/Results.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Artists from './Artists'
 import Playlists from './Playlists'
-import Songs from './Songs'
+import Tracks from './Tracks'
+import { useLocation, useNavigate} from "react-router-dom";
+
 
 function Results() {
+  const location = useLocation();
+  // console.log(location.state.artists)
+  const { artists, playlists, tracks } = location.state
+  
+  // Render the Artists, Playlists, and Tracks components using the data
   return (
     <div>
-        <Artists/>
-        <Playlists/>
-        <Songs/>
+      <Artists data={artists} />
+      <Playlists data={playlists} />
+      <Tracks data={tracks} />
     </div>
-  )
+  );
 }
 
-export default Results
+export default Results;
