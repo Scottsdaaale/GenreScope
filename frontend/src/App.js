9835d46e61821ffe644612cb6ee5239677db36d1
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-13 17:30:12 -0400

    comment out artists import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Genres from "./Components/Genres"
-import Artists from "./Components/Artists"
+// import Artists from "./Components/Artists"
 import TopTracks from "./Components/TopTracks"
 import Results from "./Components/Results"
 import { Routes, Route} from "react-router-dom"
