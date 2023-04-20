c06cc7db3b777ad20455583bfb88b2a9e10b305d
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-19 22:59:51 -0400

    removed comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Genres from "./Components/Genres"
-// import Artists from "./Components/Artists"
 import TopTracks from "./Components/TopTracks"
 import Results from "./Components/Results"
 import { Routes, Route} from "react-router-dom"
@@ -10,12 +9,9 @@ function App() {
     <Routes>
       <Route path='/' element={<Genres />} />
       <Route path='/:genre' element={<Results />} />
-      {/* <Route path='/genres/:genre' element={<Artists />} /> */}
       <Route path='/:artist' element={<TopTracks />} />
     </Routes>
   );
 }
 
 export default App;
-
-//https://www.youtube.com/watch?v=_iOGtzRXwhI
