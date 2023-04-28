d63190150103c3aacb959abaa92df02c1e7264ec
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-28 09:45:36 -0400

    fetch data

--- frontend/src/Components/Results.js
+++ frontend/src/Components/Results.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from "react-router-dom";
 import Artists from "./Artists";
 import Playlists from "./Playlists";
 import Tracks from "./Tracks";
+import { fetchData } from '../redux/spotifyDataSlice';
 
 function Results() {
+  const dispatch = useDispatch();
+  const { genre } = useParams();
+
+  const isLoading = useSelector(state => state.spotifyData.loading);
+
+  React.useEffect(() => {
+    // fetch data on component mount or when genre value changes
+    dispatch(fetchData(genre));
+  }, [dispatch, genre]);
+
   return (
     <div>
-      <Artists />
-      <Playlists />
-      <Tracks />
+      {isLoading
+        ? <div>Loading...</div>
+        : (
+          <>
+            <Artists />
+            <Playlists />
+            <Tracks />
+          </>
+        )
+      }
     </div>
   );
 }
+
 export default Results;
