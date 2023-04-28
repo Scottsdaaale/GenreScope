dad4d81cce092f8ad55e13446a7b78582efea520
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-28 09:45:57 -0400

    add thunk function

--- frontend/src/redux/spotifyDataSlice.js
+++ frontend/src/redux/spotifyDataSlice.js
@@ -1,24 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchData = createAsyncThunk(
+  'spotifyData/fetchData',
+  async (genre, thunkAPI) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": "",
+      },
+      body: JSON.stringify({ genre }),
+    };
+  
+    try {
+      const requests = ["artists", "playlists", "tracks"]
+        .map(key => fetch(`/api/${key}/`, options)
+          .then(response => response.json())
+          // access response property, e.g. data.artists, data.playlists, etc
+          .then(data => data[key])
+        );
+
+      const [artists, playlists, tracks] = await Promise.all(requests);
+      return { artists, playlists, tracks };
+    } catch(error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+    
+  }
+);
 
 const spotifyDataSlice = createSlice({
   name: 'spotifyData',
   initialState: {
+    loading: false,
     artists: [],
     playlists: [],
     tracks: [],
   },
-  reducers: {
-    setData(state, action) {
-      const { artists, playlists, tracks } = action.payload;
-      console.log("Artists:", artists);
-      console.log("Playlists:", playlists);
-      console.log("Tracks:", tracks);
-      state.artists = artists;
-      state.playlists = playlists;
-      state.tracks = tracks;
+  extraReducers: builder => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.artists = action.payload.artists;
+        state.playlists = action.payload.playlists;
+        state.tracks = action.payload.tracks;
+      })
+      .addCase(fetchData.rejected, (state) => {
+        state.loading = false;
+      });
     },
   },
-});
+);
 
-export const { setData } = spotifyDataSlice.actions;
 export default spotifyDataSlice.reducer;
