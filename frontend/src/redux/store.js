328ac5dfdad986f47b4c5cd35a351cbe1e2e8389
Scottsdaaale <scottsdaaale@gmail.com>
2023-04-28 09:46:32 -0400

    add loggermiddleware

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import spotifyDataReducer from './spotifyDataSlice';
+
+const loggerMiddleware = store => next => action => {
+  console.log('Dispatching:', action);
+  const result = next(action);
+  console.log('Next State:', store.getState());
+  return result;
+};
 
 const store = configureStore({
   reducer: {
     spotifyData: spotifyDataReducer,
   },
+  middleware: getDefaultMiddleware().concat(loggerMiddleware)
 });
 
 export default store;
