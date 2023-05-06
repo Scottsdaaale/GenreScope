2655f4fdfbfd9b0da928285be0efded958546e9b
Scottsdaaale <scottsdaaale@gmail.com>
2023-05-06 10:54:06 -0400

    added comments, formatting

--- frontend/src/redux/spotifyDataSlice.js
+++ frontend/src/redux/spotifyDataSlice.js
@@ -1,7 +1,7 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchData = createAsyncThunk(
-  'spotifyData/fetchData',
+  "spotifyData/fetchData",
   async (genre, thunkAPI) => {
     const options = {
       method: "POST",
@@ -11,48 +11,48 @@ export const fetchData = createAsyncThunk(
       },
       body: JSON.stringify({ genre }),
     };
-  
+
     try {
-      const requests = ["artists", "playlists", "tracks"]
-        .map(key => fetch(`/api/${key}/`, options)
-          .then(response => response.json())
-          // access response property, e.g. data.artists, data.playlists, etc
-          .then(data => data[key])
-        );
+      const requests = ["artists", "playlists", "tracks"].map((key) =>
+        fetch(`/api/${key}/`, options)
+          .then((response) => response.json())
+          .then((data) => data[key])
+      );
 
       const [artists, playlists, tracks] = await Promise.all(requests);
       return { artists, playlists, tracks };
-    } catch(error) {
+    } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
-    
   }
 );
 
 const spotifyDataSlice = createSlice({
-  name: 'spotifyData',
+  name: "spotifyData",
   initialState: {
     loading: false,
     artists: [],
     playlists: [],
     tracks: [],
   },
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder
+      //while the fetch is in progress loading state = true
       .addCase(fetchData.pending, (state) => {
         state.loading = true;
       })
+      //if the fetch is done then fill the empty arrays with the spotify data
       .addCase(fetchData.fulfilled, (state, action) => {
         state.loading = false;
         state.artists = action.payload.artists;
         state.playlists = action.payload.playlists;
         state.tracks = action.payload.tracks;
       })
+      //if the fetch fails, loading state = false
       .addCase(fetchData.rejected, (state) => {
         state.loading = false;
       });
-    },
   },
-);
+});
 
 export default spotifyDataSlice.reducer;
