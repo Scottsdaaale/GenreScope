a5e0a0cb68df0c30b5c46ff3da0ffe3481093252
Scottsdaaale <scottsdaaale@gmail.com>
2023-05-06 10:51:29 -0400

    render data, added loading

--- frontend/src/Components/TopTracks.js
+++ frontend/src/Components/TopTracks.js
@@ -1,30 +1,36 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 function TopTracks() {
-  const {location} = useLocation();
-  const tracks = location.state;
-  console.log(tracks);
+  const { tracks } = useSelector((state) => state.topTracksData);
+  const isLoading = useSelector((state) => state.topTracksData.loading);
 
   return (
     <div>
-      {/* {tracks.map((track) => (
-        <ul key={track.id}>
-          <li>
-            {track.name}
-            {track.preview_url ? (
-              <audio controls>
-                <source src={track.preview_url} type="audio/mpeg" />
-                Your browser does not support the audio element.
-              </audio>
-            ) : (
-              <p>No preview available.</p>
-            )}
-          </li>
-        </ul>
-      ))} */}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {tracks.map((track) => (
+            <ul key={track.id}>
+              <li>
+                {track.name}
+                {track.preview_url && (
+                  <audio controls>
+                    <source src={track.preview_url} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                )}
+              </li>
+            </ul>
+          ))}
+          {!tracks.length && (
+            <p>This artist has no available track previews.</p>
+          )}
+        </>
+      )}
     </div>
   );
 }
 
-export default TopTracks;
+export default TopTracks
