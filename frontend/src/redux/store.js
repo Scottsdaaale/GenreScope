2f7f1cd4af5a362e2865e70e6d38628761e5aade
Scottsdaaale <scottsdaaale@gmail.com>
2023-05-09 14:55:23 -0400

    refactored for redux persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,21 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import spotifyDataReducer from './spotifyDataSlice';
 import topTracksReducer from './topTracksSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    spotifyData: spotifyDataReducer,
+    topTracksData: topTracksReducer,
+  })
+);
 
 const loggerMiddleware = store => next => action => {
   console.log('Dispatching:', action);
@@ -10,11 +25,34 @@ const loggerMiddleware = store => next => action => {
 };
 
 const store = configureStore({
-  reducer: {
-    spotifyData: spotifyDataReducer,
-    topTracksData: topTracksReducer, 
-  },
-  middleware: getDefaultMiddleware().concat(loggerMiddleware)
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware().concat(loggerMiddleware),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
+
+
+
+
+// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// import spotifyDataReducer from './spotifyDataSlice';
+// import topTracksReducer from './topTracksSlice';
+
+// const loggerMiddleware = store => next => action => {
+//   console.log('Dispatching:', action);
+//   const result = next(action);
+//   console.log('Next State:', store.getState());
+//   return result;
+// };
+
+// const store = configureStore({
+//   reducer: {
+//     spotifyData: spotifyDataReducer,
+//     topTracksData: topTracksReducer, 
+//   },
+//   middleware: getDefaultMiddleware().concat(loggerMiddleware)
+// });
+
+// export default store;
