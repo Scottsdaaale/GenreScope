de306e297ea4474cf64731881329a5b5a25509c4
Scottsdaaale <scottsdaaale@gmail.com>
2023-05-20 20:02:35 -0400

    created ArtistsPage

---
+++ frontend/src/Components/ArtistsPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopTracks from './TopTracks'
+import Videos from './Videos'
+
+function ArtistsPage() {
+  return (
+    <div>
+      <TopTracks/>
+      <Videos/>
+    </div>
+  )
+}
+
+export default ArtistsPage
