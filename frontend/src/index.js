b5e3a470fd5e0da086b51d5e51183284997f4506
Scottsdaaale <scottsdaaale@gmail.com>
2023-05-27 22:12:08 -0400

    added bootstrap import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -7,6 +8,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
