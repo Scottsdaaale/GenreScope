0f5c5e113b16a113181ead894cab47daa7a5a546
Scottsdaaale <scottsdaaale@gmail.com>
2023-05-27 22:12:41 -0400

    added search function and made cards with images

--- frontend/src/Components/Genres.js
+++ frontend/src/Components/Genres.js
@@ -1,38 +1,107 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Card, Container, Form } from "react-bootstrap";
 
 function Genres() {
   const [genres, setGenres] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredGenres, setFilteredGenres] = useState([]);
 
   useEffect(() => {
     fetch("/api/genres/")
       .then((response) => response.json())
-      .then((data) => setGenres(data.genres))
+      .then((data) => {
+        setGenres(data.genres);
+        setFilteredGenres(data.genres);
+      })
       .catch((error) => console.log(error));
   }, []);
 
+  useEffect(() => {
+    // Filter genres based on search query
+    const filtered = genres.filter((genre) =>
+      genre.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredGenres(filtered);
+  }, [searchQuery, genres]);
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
-    <div className="genre-list-container">
-      <ul className="genre-list">
-        {genres.map((genre) => (
-          <li className="genre" key={genre}>
-            <Link to={`/genres/${genre}`}>
-              {genre}
-            </Link>
-          </li>
+    <Container>
+      <Form.Group controlId="searchForm">
+        <Form.Control
+          type="text"
+          placeholder="Search genres..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+      </Form.Group>
+
+      <div className="d-flex flex-wrap justify-content-center">
+        {filteredGenres.map((genre) => (
+          <Link
+            to={`/genres/${genre}`}
+            key={genre}
+            style={{ textDecoration: "none" }}
+          >
+            <Card
+              border="none"
+              style={{
+                height: "20rem",
+                width: "15rem",
+                margin: "0.5rem",
+                backgroundColor: "#181818",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              <div
+                style={{
+                  position: "relative",
+                  width: "100%",
+                  paddingTop: "100%",
+                  overflow: "hidden",
+                }}
+              >
+                <Card.Img
+                  variant="top"
+                  src="metallica2.png"
+                  alt={genre}
+                  className="rounded-circle"
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    width: "85%", // Adjust the width to make the circle smaller
+                    height: "85%", // Set the height to the same value as the width
+                    objectFit: "cover",
+                  }}
+                />
+                <div
+                style={{
+                  width: "70%",
+                  height: "70%",
+                  borderRadius: "0%",
+                  backgroundColor: "red", // Replace with desired color
+                }}
+              ></div>
+              </div>
+              <Card.Body>
+                <Card.Title style={{ color: "white" }}>{genre}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Link>
         ))}
-      </ul>
-    </div>
+      </div>
+    </Container>
   );
 }
 
 export default Genres;
-
-
-
-
-
-
 // import React, { useState, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
 // // import { csrftoken } from '../csrf';
@@ -52,7 +121,7 @@ export default Genres;
 //     const query_params = {
 //       genre: genre
 //     };
-  
+
 //     Promise.all([
 //       fetch("/api/artists/", {
 //         method: "POST",
@@ -106,4 +175,3 @@ export default Genres;
 // }
 
 // export default Genres;
-
