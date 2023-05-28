54f52e7346d7d55802263ed368ef58be29464507
Scottsdaaale <scottsdaaale@gmail.com>
2023-05-27 22:11:53 -0400

    added background styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import Genres from "./Components/Genres"
-import TopTracks from "./Components/TopTracks"
-import Results from "./Components/Results"
-import { Routes, Route} from "react-router-dom"
+import Genres from "./Components/Genres";
+import TopTracks from "./Components/TopTracks";
+import Results from "./Components/Results";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Genres />} />
-      <Route path='/genres/:genre' element={<Results />} />
-      <Route path='/:artists/:artist' element={<TopTracks />} />
-    </Routes>
+    <div style={{ backgroundColor: "#121212" /* Replace with your desired background color */, minHeight: "100vh" }}>
+      <Routes>
+        <Route path="/" element={<Genres />} />
+        <Route path="/genres/:genre" element={<Results />} />
+        <Route path="/:artists/:artist" element={<TopTracks />} />
+      </Routes>
+    </div>
   );
 }
 
