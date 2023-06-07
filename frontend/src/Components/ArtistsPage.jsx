8c7164e86d6c1900368181d6a81d7f080255d12b
Scottsdaaale <scottsdaaale@gmail.com>
2023-06-07 16:08:58 -0400

    changed component to be rendered

---
+++ frontend/src/Components/ArtistsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopTracks from './TopTracks'
+import VideosPlayer from './VideosPlayer'
+
+function ArtistsPage() {
+  return (
+    <div>
+      <TopTracks/>
+      <VideosPlayer/>
+    </div>
+  )
+}
+
+export default ArtistsPage
