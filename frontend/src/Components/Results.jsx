1930c8c26a9269c814f838979c55ddd094615e32
Scottsdaaale <scottsdaaale@gmail.com>
2023-08-09 17:08:44 -0400

    styling menu

--- frontend/src/Components/Results.jsx
+++ frontend/src/Components/Results.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, ButtonGroup, Spinner } from "react-bootstrap";
+import { Container, Button, Spinner, Dropdown, Nav } from "react-bootstrap";
 import Artists from "./Artists";
 import VideoPlayer from "./VideoPlayer";
 import Playlists from "./Playlists";
@@ -39,71 +39,153 @@ function Results() {
   };
 
   return (
-    <div className="text-center">
-      <ButtonGroup style={{ margin: "1rem" }}>
-        <Button
-          variant={
-            activeTab === "allTopResults" ? "primary" : "outline-primary"
-          }
-          onClick={handleAllTopResultsClick}
-          style={{ backgroundColor: "#17202A", borderColor: "#17202A" }}
-        >
-          Top Results
-        </Button>
-        <Button
-          variant={activeTab === "artists" ? "primary" : "outline-primary"}
-          onClick={() => handleTabClick("artists")}
-          style={{ backgroundColor: "#17202A", borderColor: "#17202A" }}
-        >
-          Artists
-        </Button>
-        <Button
-          variant={activeTab === "videos" ? "primary" : "outline-primary"}
-          onClick={() => handleTabClick("videos")}
-          style={{ backgroundColor: "#17202A", borderColor: "#17202A" }}
-        >
-          Videos
-        </Button>
-        <Button
-          variant={activeTab === "playlists" ? "primary" : "outline-primary"}
-          onClick={() => handleTabClick("playlists")}
-          style={{ backgroundColor: "#17202A", borderColor: "#17202A" }}
-        >
-          Playlists
-        </Button>
-        <Button
-          variant={activeTab === "tracks" ? "primary" : "outline-primary"}
-          onClick={() => handleTabClick("tracks")}
-          style={{ backgroundColor: "#17202A", borderColor: "#17202A" }}
-        >
-          Tracks
-        </Button>
-      </ButtonGroup>
+    <Container>
+      <div className="text-left">
+        <div style={{ paddingTop: "30px", paddingBottom: "50px" }}>
+          {/* Mobile hamburger menu */}
+          <Nav className="d-md-none justify-content-end mb-2">
+            <Dropdown as={Nav.Item}>
+              <Dropdown.Toggle
+                as={Nav.Link}
+                variant="link"
+                id="mobile-tab-dropdown"
+                style={{ color: "white", textDecoration: "none" }}
+              >
+                Menu
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  active={activeTab === "allTopResults"}
+                  onClick={handleAllTopResultsClick}
+                >
+                  Top Results
+                </Dropdown.Item>
+                <Dropdown.Item
+                  active={activeTab === "artists"}
+                  onClick={() => handleTabClick("artists")}
+                >
+                  Artists
+                </Dropdown.Item>
+                <Dropdown.Item
+                  active={activeTab === "videos"}
+                  onClick={() => handleTabClick("videos")}
+                >
+                  Videos
+                </Dropdown.Item>
+                <Dropdown.Item
+                  active={activeTab === "playlists"}
+                  onClick={() => handleTabClick("playlists")}
+                >
+                  Playlists
+                </Dropdown.Item>
+                <Dropdown.Item
+                  active={activeTab === "tracks"}
+                  onClick={() => handleTabClick("tracks")}
+                >
+                  Tracks
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
 
-      {isLoadingSpotifyGenreData || isLoadingGenreVideos ? (
-        <div
-          className="d-flex justify-content-center align-items-center"
-          style={{ minHeight: "70vh" }}
-        >
-          <Spinner animation="border" role="status" variant="light">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
+          {/* Desktop and tablet buttons */}
+          <Button
+            variant={
+              activeTab === "allTopResults" ? "primary" : "outline-primary"
+            }
+            onClick={handleAllTopResultsClick}
+            style={{
+              color: "white",
+              borderColor: "#121212",
+              backgroundColor: activeTab === "allTopResults" ? "#2a2a2a" : "#121212",
+              marginRight: "10px",
+              borderRadius: "20px",
+            }}
+            className="d-none d-md-inline-block"
+          >
+            Top Results
+          </Button>
+          <Button
+            variant={activeTab === "artists" ? "primary" : "outline-primary"}
+            onClick={() => handleTabClick("artists")}
+            style={{
+              color: "white",
+              borderColor: "#121212",
+              backgroundColor: activeTab === "artists" ? "#2a2a2a" : "#121212",
+              marginRight: "10px",
+              borderRadius: "20px",
+            }}
+            className="d-none d-md-inline-block"
+          >
+            Artists
+          </Button>
+          <Button
+            variant={activeTab === "videos" ? "primary" : "outline-primary"}
+            onClick={() => handleTabClick("videos")}
+            style={{
+              color: "white",
+              borderColor: "#121212",
+              backgroundColor: activeTab === "videos" ? "#2a2a2a" : "#121212",
+              marginRight: "10px",
+              borderRadius: "20px",
+            }}
+            className="d-none d-md-inline-block"
+          >
+            Videos
+          </Button>
+          <Button
+            variant={activeTab === "playlists" ? "primary" : "outline-primary"}
+            onClick={() => handleTabClick("playlists")}
+            style={{
+              color: "white",
+              borderColor: "#121212",
+              backgroundColor: activeTab === "playlists" ? "#2a2a2a" : "#121212",
+              marginRight: "10px",
+              borderRadius: "20px",
+            }}
+            className="d-none d-md-inline-block"
+          >
+            Playlists
+          </Button>
+          <Button
+            variant={activeTab === "tracks" ? "primary" : "outline-primary"}
+            onClick={() => handleTabClick("tracks")}
+            style={{
+              color: "white",
+              borderColor: "#121212",
+              backgroundColor: activeTab === "tracks" ? "#2a2a2a" : "#121212",
+              marginRight: "10px",
+              borderRadius: "20px",
+            }}
+            className="d-none d-md-inline-block"
+          >
+            Tracks
+          </Button>
         </div>
-      ) : (
-        <>
-          {activeTab === "allTopResults" && <TopResults />}
-          {activeTab === "artists" && <Artists />}
-          {activeTab === "videos" && <VideoPlayer />}
-          {activeTab === "playlists" && <Playlists />}
-          {activeTab === "tracks" && <Tracks />}
-        </>
-      )}
-    </div>
+        {isLoadingSpotifyGenreData || isLoadingGenreVideos ? (
+          <div
+            className="d-flex justify-content-center align-items-center"
+            style={{ minHeight: "70vh" }}
+          >
+            <Spinner animation="border" role="status" variant="light">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        ) : (
+          <>
+            {activeTab === "allTopResults" && <TopResults />}
+            {activeTab === "artists" && <Artists />}
+            {activeTab === "videos" && <VideoPlayer />}
+            {activeTab === "playlists" && <Playlists />}
+            {activeTab === "tracks" && <Tracks />}
+          </>
+        )}
+      </div>
+    </Container>
   );
 }
 
 export default Results;
-
 //BEFORE ALL TAB
 // import React, { useState } from "react";
 // import { Button, ButtonGroup, Spinner } from "react-bootstrap";
