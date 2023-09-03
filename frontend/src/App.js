fc77b42800ba8d02c1b3455e538b48da5d52fef5
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-03 13:33:17 -0400

    component name change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Genres from "./Components/Genres";
-import TopTracks from "./Components/TopTracks";
 import Results from "./Components/Results";
 import { Routes, Route } from "react-router-dom";
+import ArtistProfile from "./Components/ArtistProfile";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Genres />} />
         <Route path="/genres/:genre" element={<Results />} />
-        <Route path="/artists/:artist" element={<TopTracks />} />
+        <Route path="/artists/:artist" element={<ArtistProfile />} />
       </Routes>
     </div>
   );
